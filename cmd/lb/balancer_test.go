b9754538b3bcd119d39ecd1331b80aac805e3343
RomanShpylovyi <meisonon@gmail.com>
2021-05-16 02:30:32 +0300

    Tests

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,33 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+	. "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestBalancer(c *C) {
+	c.Assert(chooseServer([]Server{}), Equals, -1)
+	c.Assert(chooseServer([]Server{
+		Server{Url: "", Alive: false, Connections: 0},
+	}), Equals, -1)
+	c.Assert(chooseServer([]Server{
+		Server{Url: "", Alive: true, Connections: 0},
+	}), Equals, 0)
+	c.Assert(chooseServer([]Server{
+		Server{Url: "", Alive: true, Connections: 20},
+		Server{Url: "", Alive: true, Connections: 5},
+		Server{Url: "", Alive: true, Connections: 10},
+	}), Equals, 1)
+	c.Assert(chooseServer([]Server{
+		Server{Url: "", Alive: true, Connections: 20},
+		Server{Url: "", Alive: false, Connections: 5},
+		Server{Url: "", Alive: true, Connections: 10},
+	}), Equals, 2)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/roman-mazur/design-practice-2-template
+module github.com/teramont/go2-lab-2
 
 go 1.15
+
+require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect

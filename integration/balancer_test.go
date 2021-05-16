15d7fda3a5c6a4eee15ba934b368ad4a6c3d4213
RomanShpylovyi <meisonon@gmail.com>
2021-05-16 17:11:11 +0300

    Integration test

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"testing"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
 
 const baseAddress = "http://balancer:8090"
@@ -13,15 +15,35 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+func Test(t *testing.T) {
+	time.Sleep(10 * time.Second)
+	TestingT(t)
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+type IntegrationSuite struct{}
+
+var _ = Suite(&IntegrationSuite{})
+
+func (s *IntegrationSuite) TestBalancer(c *C) {
+	var srv string
+	for i := 0; i < 9; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		c.Assert(err, IsNil)
+
+		c.Assert(resp.StatusCode, Equals, http.StatusOK)
+
+		if from := resp.Header.Get("lb-from"); srv == "" {
+			srv = from
+		} else {
+			c.Assert(srv, Equals, from)
+		}
+	}
+}
+
+func (s *IntegrationSuite) BenchmarkBalancer(c *C) {
+	for i := 0; i < c.N; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		c.Assert(err, IsNil)
+		c.Assert(resp.StatusCode, Equals, http.StatusOK)
+	}
 }

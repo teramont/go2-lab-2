e72a0e409a3e84c5b8343d8fb76e31c34f101d48
ShpylovyiRoman <meisonon@gmail.com>
2021-05-30 20:15:25 +0300

    Fix pathes to this repo

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -9,8 +9,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/roman-mazur/design-practice-2-template/httptools"
-	"github.com/roman-mazur/design-practice-2-template/signal"
+	"github.com/teramont/go2-lab-2/httptools"
+	"github.com/teramont/go2-lab-2/signal"
 )
 
 type Server struct {
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -8,8 +8,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/roman-mazur/design-practice-2-template/httptools"
-	"github.com/roman-mazur/design-practice-2-template/signal"
+	"github.com/teramont/go2-lab-2/httptools"
+	"github.com/teramont/go2-lab-2/signal"
 )
 
 var port = flag.Int("port", 8080, "server port")
